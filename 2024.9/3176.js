f0bc80ee18679fc7a3067e39f4ac9fe6138ac043
Nickyzj628 <1927139831@qq.com>
2024-09-07 11:26:43 +0800

    perf: 完善昨天的解题思路

--- 2024.9/3176.js
+++ 2024.9/3176.js
@@ -10,16 +10,19 @@
  * @return {number}
  */
 var maximumLength = function (nums, k) {
-	// DP，阅读理解题
-	// dp[i][l]：以nums[i]结尾且最好子序列长度不超过l的结果，0<=l<=k
-	if (nums.length < 2) return nums.length
-	let answer = 0
+	// DP，阅读理解题，找状态，求状态转移方程
+	// 子序列：允许去除某些元素，但不改变元素顺序
+	// dp[i]: 到i为止的子序列 (0<=i<nums.length)
+	// dp[i][count]: 到i为止的子序列，并且不能有count个以上不相等的相邻元素 (0<=count<=k)
+	// 枚举i，再依次求i和0~i-1之间满足0~k的好子序列
+	// return Math.max(dp[i][k])
+	let answer = 1
 	const dp = Array.from({ length: nums.length }, () => new Array(k + 1).fill(1))
 	for (let i = 1; i < nums.length; i++) {
-		for (let l = 0; l <= k; l++) {
+		for (let count = 0; count <= k; count++) {
 			for (let j = 0; j < i; j++) {
-				if (nums[i] === nums[j]) dp[i][l] = Math.max(dp[i][l], dp[j][l] + 1)
-				else if (l > 0) dp[i][l] = Math.max(dp[i][l], dp[j][l - 1] + 1)
+				if (nums[j] === nums[i]) dp[i][count] = Math.max(dp[i][count], dp[j][count] + 1)
+				else if (count > 0) dp[i][count] = Math.max(dp[i][count], dp[j][count - 1] + 1)
 			}
 		}
 		answer = Math.max(answer, dp[i][k])
@@ -31,4 +34,6 @@ var maximumLength = function (nums, k) {
 console.log(maximumLength([1, 2, 1, 1, 3], 2)) // 4
 
 // 最长好子序列为 [1,1]
-// console.log(maximumLength([1, 2, 3, 4, 5, 1], 0)) // 2
+console.log(maximumLength([1, 2, 3, 4, 5, 1], 0)) // 2
+
+console.log(maximumLength([2], 0)) // 1
