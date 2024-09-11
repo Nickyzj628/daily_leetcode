1423ac4c538a0109b1bb0cb1e7024d615709b09c
Nickyzj628 <1927139831@qq.com>
2024-09-11 12:30:02 +0800

    perf: 优化二分查找

--- essentials/binarySearch.js
+++ essentials/binarySearch.js
@@ -3,16 +3,15 @@
  * @param {Number[]} arr
  * @param {Number} target
  */
-function binarySearch(arr, target) {
+export function binarySearch(arr, target) {
 	let left = 0
 	let right = arr.length - 1
 	while (left <= right) {
 		const mid = Math.floor((left + right) / 2)
 		if (arr[mid] < target) left = mid + 1
-		else if (arr[mid] > target) right = mid - 1
-		else return mid
+		else right = mid - 1
 	}
-	return -1
+	return left
 }
 
-console.log(binarySearch([0, 3, 5, 5, 6, 7, 8], 7))
+// console.log(binarySearch([0, 3, 5, 5, 6, 7, 8], 7))
