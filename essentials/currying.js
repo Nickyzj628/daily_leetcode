62c70a255dc5976d7dcbf7a18ae24f4ea70eff82
Nickyzj628 <1927139831@qq.com>
2024-09-25 15:05:57 +0800

    feat: 完成函数科里化

---
+++ essentials/currying.js
@@ -0,0 +1,16 @@
+/**
+ * 函数科里化求和
+ * @param  {...number[]} nums
+ */
+function sum(...nums) {
+	const calculate = (...nextNums) => {
+		nums.push(...nextNums)
+		return calculate
+	}
+	calculate.sumof = () => {
+		return nums.reduce((ansr, num) => ansr + num, 0)
+	}
+	return calculate
+}
+
+console.log(sum(1, 2, 3)(4)(5).sumof()) // 15
