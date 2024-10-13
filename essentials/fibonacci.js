ea0ed158c19f6c6596018ba681bd50c890347883
Nickyzj628 <1927139831@qq.com>
2024-10-13 10:08:51 +0800

    feat: 完成斐波那契数列

---
+++ essentials/fibonacci.js
@@ -0,0 +1,13 @@
+/**
+ * 斐波那契数列
+ * @param {number} n
+ */
+function fibonacci(n) {
+    const dp = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+
+console.log(fibonacci(10));
